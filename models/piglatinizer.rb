06856eefb072933f2ea33cfd6df03c9216e95f95
Steven Senkowski <scsenkowski@gmail.com>
2018-01-05 20:33:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -22,7 +22,7 @@ class PigLatinizer
 
   def to_pig_latin(phrase)
     initial_phrase = phrase.split(" ").map {|word| piglatinize(word)}.join(" ")
-    if initial_phrase[0][0] == "i"
+    if initial_phrase.downcase[0][0] == "i"
       initial_phrase
     elsif initial_phrase[0][0] == "o"
       final_phrase = initial_phrase.capitalize
