3faec614a2216e819aedd30da3979d2e902000cf
Steven Senkowski <scsenkowski@gmail.com>
2018-01-05 20:02:08 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -25,7 +25,7 @@ class PigLatinizer
       phrase.downcase.split(" ").map {|word| piglatinize(word)}.join(" ")
     else
       initial_phrase = phrase.downcase.split(" ").map {|word| piglatinize(word)}.join(" ")
-      initial_phrase[1].capitalize
+      initial_phrase.capitalize
     end
   end
 
