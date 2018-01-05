e7ca4ea192c4462894189cd55aba6af4416c3acb
Steven Senkowski <scsenkowski@gmail.com>
2018-01-05 20:15:23 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -24,7 +24,7 @@ class PigLatinizer
     initial_phrase = phrase.downcase.split(" ").map {|word| piglatinize(word)}.join(" ")
     if initial_phrase[0][0] == %w[a e i o u A E I O U]
       initial_phrase
-    else
+    elsif
       final_phrase = initial_phrase.capitalize
       final_phrase
       binding.pry
