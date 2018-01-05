52d5b56561ce8c213c763ccf5552e4ad87c4b9c1
Steven Senkowski <scsenkowski@gmail.com>
2018-01-05 20:03:27 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -21,7 +21,7 @@ class PigLatinizer
   end
 
   def to_pig_latin(phrase)
-    phrase.downcasesplit(" ").map {|word| piglatinize(word)}.join(" ")
+    phrase.split(" ").map {|word| piglatinize(word)}.join(" ")
   end
 
 end
